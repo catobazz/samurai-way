d87e2aaf0105265b305ca1ad9206d20d32e964f5
ArtemR <catobazzz@gmail.com>
2023-06-13 14:56:15 +0300

    1 commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,33 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="appWrapper">
+            <header className='header'>
+                <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" alt="logo"/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a> Profile </a>
+                </div>
+                <div>
+                    <a> Messages </a>
+                </div>
+                <div>
+                    <a> News </a>
+                </div>
+                <div>
+                    <a> Music </a>
+                </div>
+                <div>
+                    <a> Settings </a>
+                </div>
+            </nav>
+            <div className='content'>
+                Main Content
+            </div>
+        </div>
+    );
 }
 
 export default App;
