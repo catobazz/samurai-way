fb279194a9414cda0aaaed8730a190b2bc399bcb
Artem Rybin <catobazz@gmail.com>
2023-06-14 21:50:03 +0300

    upd

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import MyPosts from "./My Posts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img
                     src="https://storage.theoryandpractice.ru/tnp/uploads/image_unit/000/156/586/image/base_87716f252d.jpg"
