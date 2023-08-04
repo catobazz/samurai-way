da2a58ecabe35c4240ca27e7f0770eb16e1c84a2
Artem Rubin <catobazzz@gmail.com>
2023-08-04 11:21:41 +0300

    _21_dialogs.css

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Bobba
+                </div>
+                <div className={s.dialog}>
+                    Bibba
+                </div>
+                <div className={s.dialog}>
+                    Puppa
+                </div>
+                <div className={s.dialog}>
+                    Luppa
+                </div>
+                <div className={s.dialog}>
+                    Ahmed
+                </div>
+
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hello naggers!</div>
+                <div className={s.dialog}>Oba-boba!</div>
+                <div className={s.dialog}>Wuzzup! How are you?</div>
+            </div>
         </div>
     );
 };
